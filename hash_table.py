fe12f29116e3636212b7efb614d324ae1e7bf957
ghansham01 <bindumadhavshrotriy@gmail.com>
2025-07-26 11:24:28 +0530

    some more fandamental are clear

---
+++ hash_table.py
@@ -0,0 +1,10 @@
+# hash set
+s = set()
+print(s)
+
+# add item into set - O(1)
+s.add(1)
+s.add(2)
+s.add(3)
+
+print(s)
--- linked_list.py
+++ linked_list.py
@@ -26,12 +26,58 @@ B.next =C
 
 # Display linked list -> O(n)
 def display(head):
-    curr = head # curr ->it is a pointer that store the val of head 
-    element =[]
+  curr = head  # curr ->it is a pointer that store the val of head 
+  elements = []
+  while curr:
+    elements.append(str(curr.val))
+    curr = curr.next
+  print(' -> '.join(elements))
+
+# display(Head)
+
+# Search for node value - O(n)
+
+def search(head, val):
+    curr = head
     while curr:
-        element.append(str(curr.val))
-        curr = curr.next()
-    print(' -> '.join(element))
+        if val == curr.val:
+            print(True)
 
-display(Head)
+        curr = curr.next
+    return False
 
+# print('this is search')
+# search(Head, 4)
+
+# Doubly Linked Lists
+class DoublyNode:
+  def __init__(self, val, next=None, prev=None):
+    self.val = val
+    self.next = next
+    self.prev = prev
+
+  def __str__(self):
+    return str(self.val)
+
+head1 = tail = DoublyNode(1)
+print(tail)
+
+# Display - O(n)
+def display(head):
+  curr = head
+  elements = []
+  while curr:
+    elements.append(str(curr.val))
+    curr = curr.next
+  print(' <-> '.join(elements))
+
+display(head1)
+
+# Insert at beginning - O(1)
+def insert_at_beginning(head, tail, val):
+  new_node = DoublyNode(val, next=head)
+  head.prev = new_node
+  return new_node, tail
+
+head2, tail = insert_at_beginning(head1, tail, 3)
+display(head2)
