163d692ad5be5cbbc78c5c58845f0c7cbaac1f5c
ghansham01 <bindumadhavshrotriy@gmail.com>
2025-09-09 16:53:35 +0530

    one XP

---
+++ 3517Q.py
@@ -0,0 +1,12 @@
+def shortPath(x:int,y:int,z:int):
+    d1 = abs(x-z)
+    d2 = abs(y-z)
+
+    if d1<d2:
+        return 1
+    elif d1>d2:
+        return 2
+    else:
+        return 0
+
+print(shortPath(2,5,6))
