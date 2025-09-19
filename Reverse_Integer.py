bb9ab3caf68f455992ffedacb879f189986bfe85
ghansham01 <bindumadhavshrotriy@gmail.com>
2025-09-19 23:35:47 +0530

    hmm

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,5 +1,16 @@
 def reverse(n):
     ans =0
+    sign = -1 if x < 0 else 1
+    x = abs(x)
     while n!=0:
-         digit =n%10
-         if ans> 
+        digit =n%10
+        if ans> (2**31 - 1) // 10:
+            return 0
+        
+        ans = ans *10+digit
+        x//=10
+
+    ans *= sign
+    if ans < -2**31 or ans > 2**31 - 1:
+        return 0
+    return ans
