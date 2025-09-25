de263365d396d8e2aaa0f456040662613e78fe6f
ghansham01 <bindumadhavshrotriy@gmail.com>
2025-09-25 12:59:58 +0530

    Qustion 120

--- tringle.py
+++ tringle.py
@@ -1,14 +1,15 @@
-def minimumTotal(triangle: List[List[int]]) -> int:
+def minimumTotal(self, triangle):
     n = len(triangle)
-    if n == 1:
+
+    if n==1:
         return triangle[0][0]
         
-    for i in range(n - 2, -1, -1):
+    for i in range(n-2,-1,-1):
         for j in range(len(triangle[i])):
             min_below = min(triangle[i+1][j], triangle[i+1][j+1])
 
             triangle[i][j] += min_below
-            
+        
     return triangle[0][0]
 
 triangle_input = [[2],[3,4],[6,5,7],[4,1,8,3]]
