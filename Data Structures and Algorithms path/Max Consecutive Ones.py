0a2e16526cddac168d95ef343e74a5e4a8184c90
ghansham01 <bindumadhavshrotriy@gmail.com>
2026-03-25 11:28:56 +0530

    your message

---
+++ Data Structures and Algorithms path/Max Consecutive Ones.py
@@ -0,0 +1,19 @@
+def findMaxConsecutiveOnes(nums):
+    max_count = 0
+    current_count = 0
+    
+    for num in nums:
+        
+        if num == 1:
+            current_count += 1
+        
+        else:
+            if current_count > max_count:
+                max_count = current_count
+            
+            current_count = 0
+            
+    return max(max_count, current_count)
+
+
+print(findMaxConsecutiveOnes(nums=[1,1,1,0,1,0,1,0,1]))
---
+++ Data Structures and Algorithms path/Shuffle the Array.py
@@ -0,0 +1,13 @@
+def suffle(nums, n):
+    res =[]
+
+    for i in range(n):
+        res.append(nums[i])
+
+        res.append(nums[i+n])
+    
+    return res
+
+
+print(suffle(nums=[2,5,1,3,4,7], n=3))
+
